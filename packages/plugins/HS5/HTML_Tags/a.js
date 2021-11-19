c67b7079bff7ba8e11cea2ca0f0f2b76b1d94190
Year 12 <year12@RANDWICKs-MacBook-Air-2.local>
2021-11-19 10:11:37 +0000

    added homepage

--- packages/plugins/HS5/HTML_Tags/a.js
+++ packages/plugins/HS5/HTML_Tags/a.js
@@ -1,15 +1,17 @@
 const { v4: uuidv4 } = require('uuid');
 module.exports = {
     Command: function(input){
+        let uuid = uuidv4(); 
         let Parent = input[0]
         let className = input[1]
-        input.splice(0,2)
+        let href = input[2]
+        input.splice(0,3)
         let Children = input.join(";")
         let secondPart = ""
         if(Children !== ""){
           secondPart = `((parent) => {${Children}})(InternalUUID)`
         }
-        return(`(() => {let InternalUUID = uuidv4(); let ElementWeWant = ReactfulElement('button',${Parent},parent, InternalUUID, ${className}); components.push(ElementWeWant); ${secondPart}})();`)
+        return(`(() => {let InternalUUID = uuidv4(); let ElementWeWant = ReactfulElement('button',${Parent},parent, InternalUUID, ${className}); let NewElement = ReactfulElement('a',${Parent},parent, "${uuidv4()}", "hidden_tag"); components.push(ElementWeWant); NewElement.Element.href = ${href}; MontiorInputs(InternalUUID, "click", (value) => { if (!(${href} === window.location.pathname)) { history.move(${href});} });; ${secondPart}})();`)
     },
     Dependencies: function(){
       return(false)
